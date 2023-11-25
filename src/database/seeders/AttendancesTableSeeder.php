15d9a964828ee39b4225f755882bb5e90b11119e
JonyTask <yuusu.hyb@gmail.com>
2023-11-25 19:08:55 +0900

    打刻画面　管理画面　画像埋め込み

--- src/database/seeders/AttendancesTableSeeder.php
+++ src/database/seeders/AttendancesTableSeeder.php
@@ -15,7 +15,15 @@ class AttendancesTableSeeder extends Seeder
     public function run()
     {
         $param = [
-            'user_id' => 3,
+            'user_id' => 1,
+            'work_date'=>'2023-11-24',
+            'start_work'=>'2023-11-24 12:00:00',
+            'finish_work'=>'2023-11-24 18:30:00',
+        ];
+        DB::table('attendances')->insert($param);
+
+        $param = [
+            'user_id' => 2,
             'work_date'=>'2023-11-24',
             'start_work'=>'2023-11-24 18:00:00',
             'finish_work'=>'2023-11-24 23:30:00',
@@ -23,7 +31,7 @@ class AttendancesTableSeeder extends Seeder
         DB::table('attendances')->insert($param);
 
         $param = [
-            'user_id' => 4,
+            'user_id' => 3,
             'work_date'=>'2023-11-24',
             'start_work'=>'2023-11-24 13:00:00',
             'finish_work'=>'2023-11-24 19:30:00',
@@ -31,7 +39,7 @@ class AttendancesTableSeeder extends Seeder
         DB::table('attendances')->insert($param);
 
         $param = [
-            'user_id' => 5,
+            'user_id' => 4,
             'work_date'=>'2023-11-24',
             'start_work'=>'2023-11-24 15:00:00',
             'finish_work'=>'2023-11-24 21:00:00',
@@ -39,10 +47,18 @@ class AttendancesTableSeeder extends Seeder
         DB::table('attendances')->insert($param);
 
         $param = [
-            'user_id' => 6,
+            'user_id' => 5,
             'work_date'=>'2023-11-24',
             'start_work'=>'2023-11-24 10:00:00',
             'finish_work'=>'2023-11-24 18:00:00',
+        ];
+        DB::table('attendances')->insert($param);
+
+        $param = [
+            'user_id' => 6,
+            'work_date'=>'2023-11-24',
+            'start_work'=>'2023-11-24 19:00:00',
+            'finish_work'=>'2023-11-24 23:20:00',
         ];
         DB::table('attendances')->insert($param);
     }
--- src/database/seeders/RestsTableSeeder.php
+++ src/database/seeders/RestsTableSeeder.php
@@ -15,37 +15,44 @@ class RestsTableSeeder extends Seeder
     public function run()
     {
         $param = [
+            'attendance_id' => 1,
+            'start_rest' => '2023-11-24 14:30:00',
+            'finish_rest' => '2023-11-24 14:45:00'
+        ];
+        DB::table('rests')->insert($param);
+
+        $param = [
+            'attendance_id' => 2,
+            'start_rest' => '2023-11-24 20:30:00',
+            'finish_rest' => '2023-11-24 21:00:00'
+        ];
+        DB::table('rests')->insert($param);
+
+        $param = [
             'attendance_id' => 3,
-            'start_rest' => '2023-11-24 21:00:00',
-            'finish_rest' => '2023-11-24 21:15:00'
-        ];
-        DB::table('rests')->insert($param);
-
-        $param = [
-            'attendance_id' => 4,
-            'start_rest' => '2023-11-24 17:30:00',
-            'finish_rest' => '2023-11-24 17:50:00'
-        ];
-        DB::table('rests')->insert($param);
-
-        $param = [
-            'attendance_id' => 5,
             'start_rest' => '2023-11-24 18:00:00',
             'finish_rest' => '2023-11-24 18:10:00'
         ];
         DB::table('rests')->insert($param);
 
         $param = [
-            'attendance_id' => 6,
+            'attendance_id' => 4,
             'start_rest' => '2023-11-24 16:00:00',
             'finish_rest' => '2023-11-24 16:20:00'
         ];
         DB::table('rests')->insert($param);
 
         $param = [
-            'attendance_id' => 4,
-            'start_rest' => '2023-11-24 18:30:00',
-            'finish_rest' => '2023-11-24 18:40:00'
+            'attendance_id' => 5,
+            'start_rest' => '2023-11-24 13:30:00',
+            'finish_rest' => '2023-11-24 14:00:00'
+        ];
+        DB::table('rests')->insert($param);
+
+        $param = [
+            'attendance_id' => 6,
+            'start_rest' => '2023-11-24 21:30:00',
+            'finish_rest' => '2023-11-24 21:50:00'
         ];
         DB::table('rests')->insert($param);
     }
