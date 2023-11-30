e7dd0ab178839904cc3de9bac4263f00e48f7ea5
JonyTask <yuusu.hyb@gmail.com>
2023-11-30 16:21:10 +0900

    ユーザ別ページのコントローラ追記

--- src/app/Http/Controllers/UserDivideController.php
+++ src/app/Http/Controllers/UserDivideController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserDivideController extends Controller
 {
-    //
+    public function EachUserView(Request $request){
+        $viewUser = User::where('name',$request)->Paginate(5);
+        return view('eachUser',compact('viewUser'));
+    }
 }
