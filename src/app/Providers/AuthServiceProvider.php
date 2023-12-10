6aad67ba84f806b524a4dd82f3d1cda3858ab68d
JonyTask <yuusu.hyb@gmail.com>
2023-12-10 16:00:19 +0900

    mailtrap認証

--- src/app/Providers/AuthServiceProvider.php
+++ src/app/Providers/AuthServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Auth\Notifications\VerifyEmail;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -24,7 +27,5 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        //
     }
 }
